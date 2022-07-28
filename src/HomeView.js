2b8e8e9058eb7764ec37ed8eca04205a5f286bed
nemesysolano@gmail.com <nemesysolano@gmail.com>
2022-07-27 22:52:56 -0400

    Fixed urls

--- src/HomeView.js
+++ src/HomeView.js
@@ -14,7 +14,7 @@ export const HomeView = ({navigation}) => {
 
         <View>
           <Button title="Video" onPress={()=>navigation.navigate('Video', {url: 'http://localhost:9090/video?source=https://www.w3schools.com/html/mov_bbb.mp4'})} />
-          <Button title="PDF" onPress={()=>navigation.navigate('PDF', {url: 'http://localhost:9090/document?source=https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'})} />
+          <Button title="PDF" onPress={()=>navigation.navigate('PDF', {url: 'http://localhost:9090/document.pdf?source=https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'})} />
         </View>
 
   );
--- src/PDFView.js
+++ src/PDFView.js
@@ -7,9 +7,8 @@ export const PDFView = ({navigation, route}) => {
     return (
         <WebView
             bounces={true}
-            useWebKit={true}
             scrollEnabled={true}
-          source={{ uri: 'http://docs.google.com/gview?embedded=true&url=http://www.africau.edu/images/default/sample.pdf' }} />
+          source={{ uri: route.params.url }} />
     )
 }
 
